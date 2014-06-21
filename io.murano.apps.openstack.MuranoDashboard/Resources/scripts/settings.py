d494d80ccb9da2379995668015c572a75ffeae78
Georgy Okrokvertskhov <gokrokvertskhov@mirantis.com>
2014-06-21 10:10:22 -0700

    Initial version of Murano Dashboard app
    This application installs Murano dashboard component to existing
    VM with Murano API. It does not create a new VM, so it requires to have an existing
    Murano API&Engine application in Murano Environment.

---
+++ io.murano.apps.openstack.MuranoDashboard/Resources/scripts/settings.py
@@ -0,0 +1,171 @@
+import logging
+import os
+import sys
+import tempfile
+
+from openstack_dashboard import exceptions
+
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+BIN_DIR = os.path.abspath(os.path.join(ROOT_PATH, '..', 'bin'))
+
+if ROOT_PATH not in sys.path:
+    sys.path.append(ROOT_PATH)
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+METADATA_CACHE_DIR = os.path.join('/tmp',
+                                  'muranodashboard-cache')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join('/tmp', 'openstack-dashboard.sqlite')
+    }
+}
+
+SITE_BRANDING = 'OpenStack Dashboard'
+
+LOGIN_URL = '/auth/login/'
+LOGOUT_URL = '/auth/logout/'
+# LOGIN_REDIRECT_URL can be used as an alternative for
+# HORIZON_CONFIG.user_home, if user_home is not set.
+# Do not set it to '/home/', as this will cause circular redirect loop
+LOGIN_REDIRECT_URL = '/'
+
+MEDIA_ROOT = os.path.abspath(os.path.join(ROOT_PATH, '..', 'media'))
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.abspath(os.path.join(ROOT_PATH, '..', 'static'))
+STATIC_URL = '/static/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+ROOT_URLCONF = 'openstack_dashboard.urls'
+
+HORIZON_CONFIG = {
+    'dashboards': ('project', 'admin', 'settings', 'murano'),
+    'default_dashboard': 'murano',
+    'user_home': 'muranodashboard.views.get_user_home',
+    'ajax_queue_limit': 10,
+    'auto_fade_alerts': {
+        'delay': 3000,
+        'fade_duration': 1500,
+        'types': ['alert-success', 'alert-info']
+    },
+    'help_url': "http://docs.openstack.org",
+    'exceptions': {'recoverable': exceptions.RECOVERABLE,
+                   'not_found': exceptions.NOT_FOUND,
+                   'unauthorized': exceptions.UNAUTHORIZED},
+}
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'muranodashboard.middleware.ExceptionMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.request',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.contrib.messages.context_processors.messages',
+    'horizon.context_processors.horizon',
+    'openstack_dashboard.context_processors.openstack'
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    'horizon.loaders.TemplateLoader',
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(ROOT_PATH, 'templates'),
+)
+
+STATICFILES_FINDERS = (
+    'compressor.finders.CompressorFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+MAX_FILE_SIZE_MB = 5
+
+less_binary = os.path.join(BIN_DIR, 'less', 'lessc')
+COMPRESS_PRECOMPILERS = (
+    ('text/less', (less_binary + ' {infile} {outfile}')),
+)
+
+COMPRESS_CSS_FILTERS = (
+    'compressor.filters.css_default.CssAbsoluteFilter',
+)
+
+COMPRESS_ENABLED = True
+COMPRESS_OUTPUT_DIR = 'muranodashboard'
+COMPRESS_CSS_HASHING_METHOD = 'hash'
+COMPRESS_PARSER = 'compressor.parser.HtmlParser'
+
+INSTALLED_APPS = (
+    'openstack_dashboard',
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'compressor',
+    'horizon',
+    'openstack_dashboard.dashboards.project',
+    'openstack_dashboard.dashboards.admin',
+    'openstack_dashboard.dashboards.settings',
+    'openstack_auth',
+    'floppyforms',
+    'muranodashboard'
+)
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+AUTHENTICATION_BACKENDS = ('openstack_auth.backend.KeystoneBackend',)
+MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+SESSION_COOKIE_HTTPONLY = True
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+SESSION_COOKIE_SECURE = False
+
+SECRET_KEY = 'some_random_value'
+
+gettext_noop = lambda s: s
+LANGUAGES = (
+    ('en', gettext_noop('English')),
+    ('it', gettext_noop('Italiano')),
+    ('es', gettext_noop('Spanish')),
+    ('fr', gettext_noop('French')),
+    ('ja', gettext_noop('Japanese')),
+    ('pt', gettext_noop('Portuguese')),
+    ('pl', gettext_noop('Polish')),
+    ('zh-cn', gettext_noop('Simplified Chinese')),
+    ('zh-tw', gettext_noop('Traditional Chinese')),
+)
+LANGUAGE_CODE = 'en'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+OPENSTACK_KEYSTONE_DEFAULT_ROLE = 'Member'
+
+DEFAULT_EXCEPTION_REPORTER_FILTER = 'horizon.exceptions.HorizonReporterFilter'
+
+try:
+    from local.local_settings import *  # noqa
+except ImportError:
+    logging.warning("No local_settings file found.")
+
+if DEBUG:
+    logging.basicConfig(level=logging.DEBUG)
+
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
---
+++ io.murano.apps.openstack.MuranoDashboard/Resources/scripts/startMurano.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+curr_dir=$(cd $(dirname "$0") && pwd)
+
+function include(){
+    curr_dir=$(cd $(dirname "$0") && pwd)
+    inc_file_path=$curr_dir/$1
+    if [ -f "$inc_file_path" ]; then
+        . $inc_file_path
+    else
+        echo -e "$inc_file_path not found!"
+        exit 1
+    fi
+}
+include "common.sh"
+
+bash installer.sh -p sys -i "nodejs"
+
+cd /root
+
+#git clone https://github.com/openstack/horizon
+wget -q --no-check-certificate https://dl.dropboxusercontent.com/u/486062/horizon-2014.1.dev1.g8d0b69d.tar.gz
+
+
+tar xzf horizon-2014.1.dev1.g8d0b69d.tar.gz
+cd horizon-2014.1.dev1.g8d0b69d
+sudo pip install -r requirements.txt
+sudo python setup.py install
+
+cd ..
+
+git clone https://github.com/stackforge/murano-dashboard
+cd murano-dashboard
+
+sudo pip install -r requirements.txt
+
+
+file=muranodashboard/local/local_settings.py
+cp -f $curr_dir/settings.py muranodashboard/
+cp -f $curr_dir/local_settings.py muranodashboard/local/
+
+sed -i.bak "s/%KEYSTONE_IP%/$1/g" $file
+
+python setup.py install
+
+python manage.py syncdb --noinput
+
+screen -d -m python manage.py runserver 0.0.0.0:8080
+
+
+#add_fw_rule '-I INPUT 1 -p tcp -m tcp --dport 8080 -j ACCEPT -m comment --comment "by murano, CloudFoundry"'
+
+
