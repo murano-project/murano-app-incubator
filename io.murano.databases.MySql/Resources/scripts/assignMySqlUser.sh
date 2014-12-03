62df655d90744c330433089dac5d6c24bf3df14c
Dmytro Dovbii <ddovbii@mirantis.com>
2014-12-03 19:11:48 +0200

    Update Sql apps

--- io.murano.databases.MySql/Resources/scripts/assignMySqlUser.sh
+++ io.murano.databases.MySql/Resources/scripts/assignMySqlUser.sh
@@ -2,3 +2,4 @@
 
 mysql --user=root --password=root -e "GRANT ALL PRIVILEGES ON $1.* TO '$2'@'localhost' WITH GRANT OPTION"
 mysql --user=root --password=root -e "GRANT ALL PRIVILEGES ON $1.* TO '$2'@'%' WITH GRANT OPTION"
+mysql --user=root --password=root -e "FLUSH PRIVILEGES"
