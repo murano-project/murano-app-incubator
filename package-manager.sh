d3f661eb7549053bbb8d9f8b802d06fa384f8663
Ekaterina Chernova <efedorova@mirantis.com>
2014-12-08 18:15:55 +0300

    Update package-manager script

--- package-manager.sh
+++ package-manager.sh
@@ -6,38 +6,36 @@ MURANO_IP="localhost"
 MURANO_PORT="8082"
 
 # FUNCTIONS
-function check_succsess {
-    if [ $? -ne 0 ]; then
-        echo "...can't finish copy operation"
-        exit 1
-    else
-        echo "...SUCCESS"
-    fi
+function die {
+    echo "$@"
+    exit 1
 }
 
-function create_package {
+
+function check_and_create_package {
     local package=$1
-
-    if ! file --mime-type "$package" | grep -q "application/zip"; then
-        if [[ -z "$package" ]]; then
-            echo "No directory or zip file provided."
-            exit
+    if [[ -z "$package" ]]; then
+        die "No directory or zip file provided."
+    fi    
+    if [[ -f "$package" ]]; then
+        if file --mime-type "$package" | grep -q "application/zip"; then
+            return
+        else
+            die "File and not zip"
         fi
-
-        if [[ ! -d "${package}" ]]; then
-            echo "Folder '${package}' doesn't exist."
-            exit
-        fi
+    fi
+        
+    if [[ -d "${package}" ]]; then
         # Remove ending slash
         package=${package%/*}
 
         pushd ${package}
 
-        PACKAGE="${SCRIPT_DIR}/${PACKAGE}.zip"
-        echo "NAME = ${PACKAGE}"
-        zip -r -q ${PACKAGE} *
-        check_succsess
+        PACKAGE="${SCRIPT_DIR}/${package}.zip"
+        zip -r -q ${PACKAGE} * || die "Can't create zip archive"
         popd
+    else
+        die "Folder '${package}' doesn't exist."
     fi
 }
 
@@ -57,8 +55,7 @@ Usage: upload.sh zip_package|directory_name [--rc]|[--tenant-name][--murano-ip][
 "
 
 if [ "$*" == "" ] || [ "$*" == "-h" ] || [ "$*" == "--help" ]; then
-    echo "$usage"
-    exit 1
+    die "$usage"
 fi
 
 PACKAGE="$1"
@@ -83,20 +80,18 @@ do
             shift
             ;;
         *)
-            echo "$usage"
-            exit 1
+            die "$usage"
             ;;
     esac
 done
 
-create_package ${PACKAGE}
+check_and_create_package ${PACKAGE}
 echo "PACKAGE = ${PACKAGE}"
 echo "RC_FILE = ${RC_FILE}"
 
 source ${RC_FILE}
-check_succsess
+
 sudo apt-get -y -q install python-pip
 sudo pip -q install python-muranoclient
 
-murano --murano-url http://${MURANO_IP}:${MURANO_PORT} package-import ${PACKAGE}
-check_succsess
+murano --murano-url http://${MURANO_IP}:${MURANO_PORT} package-import ${PACKAGE} || die "Unable to import package '${PACKAGE}'"
