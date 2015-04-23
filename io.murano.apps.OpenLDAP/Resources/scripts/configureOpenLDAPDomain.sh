f2d1247089d52a115c889145b43b858ddb240e09
greg pryzby <gpryzby@mirantis.com>
2015-04-23 15:10:57 -0400

    v0.1 of OpenLDAP

---
+++ io.murano.apps.OpenLDAP/Resources/scripts/configureOpenLDAPDomain.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+DOMAIN="$1"
+
+echo "reconfigure slapd" > /tmp/configureDomain.out
+echo $DOMAIN >> /tmp/configureDomain.out
+
+echo "slapd slapd/no_configuration boolean false"   |  debconf-set-selections
+echo "slapd slapd/domain string ${DOMAIN}"          |  debconf-set-selections
+echo "slapd shared/organization string '${DOMAIN}'" |  debconf-set-selections
+echo "slapd slapd/password1 password openstack"     |  debconf-set-selections
+echo "slapd slapd/password2 password openstack"     |  debconf-set-selections
+echo "slapd slapd/backend select HDB"               |  debconf-set-selections
+echo "slapd slapd/purge_database boolean true"      |  debconf-set-selections
+echo "slapd slapd/allow_ldap_v2 boolean false"      |  debconf-set-selections
+echo "slapd slapd/move_old_database boolean true"   |  debconf-set-selections
+dpkg-reconfigure -f noninteractive slapd
---
+++ io.murano.apps.OpenLDAP/Resources/scripts/configureOpenLDAPUser.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+DOMAIN="$1"
+USERNAME="$2"
+PASSWORD="$3"
+
+NAME="`echo "$DOMAIN" | cut -d. -f1`"
+TLD="`echo "$DOMAIN" | cut -d. -f2`"
+
+echo "configure User" > /tmp/configureUser.out
+echo $NAME $USERNAME $PASSWORD >> /tmp/configureUser.out
+
+# Create group.ldif and user.ldif
+cat << GROUP > /tmp/group.ldif
+dn: ou=users,dc=${NAME},dc=${TLD}
+objectClass: top
+objectClass: organizationalUnit
+GROUP
+
+cat << USER > /tmp/user.ldif
+dn: uid=${USERNAME},ou=users,dc=${NAME},dc=${TLD}
+objectClass: top
+objectClass: account
+objectClass: posixAccount
+objectClass: shadowAccount
+cn: ${USERNAME}
+uid: ${USERNAME}
+uidNumber: 1001
+gidNumber: 1001
+homeDirectory: /home/${USERNAME}
+loginShell: /bin/bash
+gecos: ${USERNAME}
+userPassword: {crypt}x
+mail: ${USERNAME}@${DOMAIN}
+shadowLastChange: 0
+shadowMax: 0
+shadowWarning: 0
+USER
+
+ldapadd -x -w openstack -D "cn=admin,dc=${NAME},dc=${TLD}" -f /tmp/group.ldif
+ldapadd -x -w openstack -D "cn=admin,dc=${NAME},dc=${TLD}" -f /tmp/user.ldif
+ldappasswd -s ${PASSWORD} -D "cn=admin,dc=${NAME},dc=${TLD}" -w openstack -x uid=${USERNAME},ou=users,dc=${NAME},dc=${TLD}
