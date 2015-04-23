33fac098a3b90b95728af9e23f2b7068894dc5e2
greg pryzby <gpryzby@mirantis.com>
2015-04-23 15:11:50 -0400

    v0.1 of Jenkins

--- io.murano.apps.Jenkins/Resources/scripts/deployJenkins.sh
+++ io.murano.apps.Jenkins/Resources/scripts/deployJenkins.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
 
 #install requirements
-sudo wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get update
 
 # Jenkins
-sudo apt-get -y install jenkins
+apt-get -y install jenkins
 
-sudo sed -e "s/^START=no/START=yes/" -i /etc/default/jenkins
-sudo iptables -I INPUT 1 -p tcp -m tcp --dport 8080 -j ACCEPT -m comment --comment "by Murano, Jenkins"
-sudo service jenkins restart
+sed -e "s/^START=no/START=yes/" -i /etc/default/jenkins
+iptables -I INPUT 1 -p tcp -m tcp --dport 8080 -j ACCEPT -m comment --comment "by Murano, Jenkins"
+service jenkins start
